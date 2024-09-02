b9b1636513860546f117d4aac530edb3b52a3401
Corso Rosati <corsodr@gmail.com>
2024-09-02 17:50:58 -0400

    update s3 settings

--- src/utils/uploadToS3.ts
+++ src/utils/uploadToS3.ts
@@ -1,21 +1,17 @@
 import AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
+import dotenv from 'dotenv';
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
+dotenv.config();
+
+const s3 = new AWS.S3();
 
 export async function uploadToS3(buffer: Buffer, mimeType: string): Promise<string> {
-  const key = `${uuidv4()}.jpg`;
-
   const params = {
-    Bucket: process.env.AWS_S3_BUCKET_NAME!,
-    Key: key,
+    Bucket: process.env.S3_BUCKET_NAME!,
+    Key: `${uuidv4()}`,
     Body: buffer,
     ContentType: mimeType,
-    ACL: 'public-read',
   };
 
   const data = await s3.upload(params).promise();
